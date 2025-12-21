59c5282c66d0a892da0ffef30dcb0838a6b06bd2
Nozarashi12 <adithyanadhi619@gmail.com>
2025-12-22 02:03:49 +0530

    added i

--- dentalportal/lib/db.js
+++ dentalportal/lib/db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: 'srv1130.hstgr.io',
   user: 'u198044505_adithyan',
-  password: 'SimplePass123',
+  password: 'SImplePass123',
   database: 'u198044505_dental_portal',
   port: 3306,
   waitForConnections: true,
